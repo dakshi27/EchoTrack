1053b6366040e399c263d60deeb0e70a246f9200
Dakshitha <dakshitha27.t@gmail.com>
2026-01-09 15:44:36 +0530

    Step 3: Add domain models User and Feedback

---
+++ EchoTrack/Models/Feedback.cs
@@ -0,0 +1,18 @@
+﻿namespace EchoTrack.Api.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Status { get; set; } = "Open";
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
+
---
+++ EchoTrack/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace EchoTrack.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+    }
+}
