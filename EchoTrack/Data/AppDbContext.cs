17d587ebe8657eae4d993b2d07c4022cb6491e5d
Dakshitha <dakshitha27.t@gmail.com>
2026-01-09 16:14:24 +0530

    Step 4: Add AppDbContext and SQL Server configuration

---
+++ EchoTrack/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EchoTrack.Api.Models;
+
+namespace EchoTrack.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+    }
+}
