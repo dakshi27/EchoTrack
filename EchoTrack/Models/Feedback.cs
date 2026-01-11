cea95de4073226eb93644cab8a61f4b66faa1814
Dakshitha <dakshitha27.t@gmail.com>
2026-01-11 14:57:07 +0530

    Added the Comment Line

--- EchoTrack/Models/Feedback.cs
+++ EchoTrack/Models/Feedback.cs
@@ -3,15 +3,10 @@
     public class Feedback
     {
         public int Id { get; set; }
-
         public string Title { get; set; } = null!;
-
         public string Description { get; set; } = null!;
-
         public string Status { get; set; } = "Open";
-
         public int CreatedBy { get; set; }
-
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- EchoTrack/Models/User.cs
+++ EchoTrack/Models/User.cs
@@ -3,11 +3,8 @@
     public class User
     {
         public int Id { get; set; }
-
         public string Username { get; set; } = null!;
-
-        public string PasswordHash { get; set; } = null!;
-
+        public string PasswordHash { get; set; } = null!;  //Password should be stored as a hash
         public string Role { get; set; } = null!;
     }
 }
