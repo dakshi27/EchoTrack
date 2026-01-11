1974c19f56ece8987e855490626c23318c0019d5
Dakshitha <dakshitha27.t@gmail.com>
2026-01-11 14:54:44 +0530

    Some Fixes

--- EchoTrack/Authentication/JwtTokenService.cs
+++ EchoTrack/Authentication/JwtTokenService.cs
@@ -1,7 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using EchoTrack.Api.Models;
 
