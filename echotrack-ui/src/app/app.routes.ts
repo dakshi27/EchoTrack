a24f8253ad36742c5ddc7d799663e8f522c2f249
Dakshitha <dakshitha27.t@gmail.com>
2026-01-22 11:57:58 +0530

    Auth guard implemented

--- echotrack-ui/src/app/app.routes.ts
+++ echotrack-ui/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+//import { LoginComponent } from './components/login/login.component';
+//import { FeedbackComponent } from './components/feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    //component: LoginComponent
+  },
+  {
+    path: 'feedback',
+   // component: FeedbackComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
